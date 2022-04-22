68c440bc239cc109f403b7bc0b520fbf605e53df
paragpatil187 <paragpatil187@gmail.com>
2022-04-22 23:54:08 +0530

    remove added item in cart inplace

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,36 +1,51 @@
 import { Button } from '@material-ui/core';
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteCart } from '../../redux/Data/action';
 
 const Cart = () => {
+  const [idx,setIdx]=useState("")
   const dispatch=useDispatch()
   const data = useSelector((state) => state.data.data);
   console.log("cart", data);
   const cartproducts=useSelector((state)=>state.data.cart);
  //var idx=id.id
- const handlecartDelete=(id)=>{
+//  const handleRemove=(id)=>{
+//    //alert("hii")
+//    var cartproduct=cartproducts.filter((e)=>{
+//      if(e.id!=id){
+//        return e
+//      }
+//    })
+//    console.log(cartproduct)
    //find by id
    //filter 
    //new ayrray
-  const payload={
-    id:[]
-  }
-  dispatch(deleteCart(payload))
+  
 
 
- }
+ //}
+ 
 // var cartproducts=data.filter((x)=>{
 //   if(+idx==x.id){
 //     return x
 //   }
 // })
+const handlecartDelete=()=>{
+
+}
 console.log(cartproducts)
 let x=cartproducts
   return (
     <div>
     <Button onClick={()=>handlecartDelete}>delete cart</Button>
-    {x && x.map((e)=>{
+    {x.filter((e)=>{
+      if(+idx!=+e.id){
+        return e
+
+      }
+    })
+      .map((e)=>{
       return(
         <div>
         <h1>{e.title}</h1>
@@ -38,6 +53,7 @@ let x=cartproducts
         <img src={e.images.image1} alt="producti"/>
         <br/>
         <Button>Buy Product</Button>
+        <Button onClick={()=>setIdx(e.id)}>remove item</Button>
 
         </div>
       )
